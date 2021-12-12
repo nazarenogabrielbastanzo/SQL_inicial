6c4420800c8e3fcd2f4dbab1c1196f8f8137907b
Nazareno Gabriel Bastanzo <b.nazarenogabriel@gmail.com>
2021-12-12 11:23:52 -0300

    Ejercitacion 4

--- SQL2/ejercitaciones/ejercitacion4.sql
+++ SQL2/ejercitaciones/ejercitacion4.sql
@@ -1,7 +1,13 @@
 /* Escribe una consulta que devuelva la cantidad de profesores que dictan más de un curso en el turno Noche. */
-SELECT prof.id, prof.nombre, prof.apellido, cur.turno
-from PROFESOR prof inner JOIN CURSO cur
-ON cur.turno = 'Noche'
-AND prof.id = cur.PROFESOR_id;
+SELECT count(*) FROM 
+  (SELECT PROFESOR_id FROM curso 
+    WHERE turno = 'Noche' 
+    GROUP BY PROFESOR_id 
+    HAVING count(*) > 1) AS prof;
 
 /* Escribe una consulta para obtener la información de todos los estudiantes que no realizan el curso con código 105. */
+SELECT est.nombre, est.apellido
+FROM ESTUDIANTE est 
+WHERE est.legajo NOT IN 
+	(select ESTUDIANTE_legajo FROM INSCRIPCION 
+		where CURSO_codigo = 105 );
